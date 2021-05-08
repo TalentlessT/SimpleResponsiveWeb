887f8fcf79bf959c4f899091210fa5c11088c1b3
THANOS_CAR <jan.342003@seznam.cz>
2021-05-08 20:39:50 +0200

    mobile friendly menu

---
+++ menu.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    if(document.getElementById('mobile-menu').style.right == "-100%"){
+        document.getElementById('mobile-menu').style.right = 0;
+    console.log("lol");
+    }
+    else{
+        document.getElementById('mobile-menu').style.right = "-100%";
+        console.log("XD");
+    }
+}
